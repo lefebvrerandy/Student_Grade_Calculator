0adf6c499469f4a190f34bc72cf15686e8ab9fa8
Randy Lefebvre <Rlefebvre2256@conestogac.on.ca>
2019-04-07 18:37:36 -0400

    Added the ability to dynamically create rows in the ViewGrades.java table layout. This will be used after checking the database for saved grades

--- app/src/main/java/com/example/randy_bence/student_grade_calculator/ViewGrades.java
+++ app/src/main/java/com/example/randy_bence/student_grade_calculator/ViewGrades.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 
 public class ViewGrades extends AppCompatActivity {
@@ -37,6 +40,12 @@ public class ViewGrades extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(semester + " " + year);
 
+        /* ***************************************/
+        /*  We now have semester and year that   */
+        /*  we will use to search the database   */
+        /*  in the variables semester, year      */
+        /* ***************************************/
+
         // If we dont find the toolbar, lets not load this.. If we cannot
         // find the toolbar and try to load this, we will crash the program
         if (getSupportActionBar() != null) {
@@ -52,11 +61,36 @@ public class ViewGrades extends AppCompatActivity {
             }
         });
 
+        /* ***************************************/
+        /*  We now need to find out how many       */
+        /*  rows we have in the database linking   */
+        /*  the grades that were stored  .         */
+        /*  We can then iterating through each     */
+        /*  row and place them into the tablelayout*/
+        /* *****************************************/
 
-        /* ***************************************/
-        /*  We now have semester and year that   */
-        /*  we will use to search the database   */
-        /*  in the variables semester, year      */
-        /* ***************************************/
+        // Add rows to the table layout
+        TableLayout tl = findViewById(R.id.grades_table);
+        TableRow tr = new TableRow(this);
+        TextView course = new TextView(this);;
+        TextView weight = new TextView(this);;
+        TextView finalMark = new TextView(this);;
+
+        /*  This is debug to show how to dynamically add a row          */
+        /*  Here we should check the database for each row stored       */
+        /*      Then store that information into the student class      */
+        /*      Then place that stored information into a new row           */
+        // Set labels to their values
+        course.setText(" SEF ");
+        weight.setText(" 80 ");
+        finalMark.setText(" 100 ");
+
+        // Add the textviews to the row
+        tr.addView(course);
+        tr.addView(weight);
+        tr.addView(finalMark);
+
+        // Add the row to the table layout
+        tl.addView(tr);
     }
 }
